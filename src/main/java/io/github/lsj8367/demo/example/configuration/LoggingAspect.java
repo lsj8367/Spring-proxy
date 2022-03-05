82dcd1259f8fac9b73c3e6356647fbdabb74b84b
lsj8367 <lsj8367@naver.com>
2022-03-05 14:21:19 +0900

    :sparkles: 컨트롤러 aop 추가

--- src/main/java/io/github/lsj8367/demo/example/configuration/LoggingAspect.java
+++ src/main/java/io/github/lsj8367/demo/example/configuration/LoggingAspect.java
@@ -1,7 +1,9 @@
 package io.github.lsj8367.demo.example.configuration;
 
 import lombok.extern.slf4j.Slf4j;
+import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.AfterReturning;
+import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.springframework.stereotype.Component;
@@ -11,7 +13,6 @@ import org.springframework.stereotype.Component;
 @Slf4j
 public class LoggingAspect {
 
-//    * hello*(..) 도 상관없음
     @Before("execution(* io.github.lsj8367.demo.example.web.SampleController.hello())")
     public void beforeLog() {
         log.info("✅ Before Log");
@@ -20,6 +21,15 @@ public class LoggingAspect {
     @AfterReturning("execution(* hello*(..))")
     public void afterReturningLog() {
         log.info("👍 After Log");
+    }
+
+    @Around("execution(* around*(..))")
+    public Object aroundLog(final ProceedingJoinPoint joinPoint) throws Throwable {
+        log.info("컨트롤러 메소드 시작 전");
+//        joinPoint.getArgs(); //이 arguments로 매개변수에 어떤 행위를 가할 수 있다.
+        final Object executeProcess = joinPoint.proceed();// 이부분이 실제 컨트롤러의 동작 메소드
+        log.info("컨트롤러 메소드 실행 후");
+        return executeProcess;
     }
 
 }
--- src/main/java/io/github/lsj8367/demo/example/web/SampleController.java
+++ src/main/java/io/github/lsj8367/demo/example/web/SampleController.java
@@ -10,7 +10,13 @@ public class SampleController {
 
     @GetMapping("/api/v1/hello")
     public String hello() {
-        log.info("컨트롤러 실행중");
+        log.info("hello 컨트롤러 실행");
         return "hello";
+    }
+
+    @GetMapping("/api/v1/around")
+    public String around() {
+        log.info("around 컨트롤러 실행");
+        return "around";
     }
 }
