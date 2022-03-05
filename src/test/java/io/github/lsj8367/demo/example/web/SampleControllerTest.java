5343b08abb70d42069e66448ab8767549ba422a7
lsj8367 <lsj8367@naver.com>
2022-03-05 14:21:34 +0900

    :white_check_mark: 컨트가러 테스트 케이스 추가

--- src/test/java/io/github/lsj8367/demo/example/web/SampleControllerTest.java
+++ src/test/java/io/github/lsj8367/demo/example/web/SampleControllerTest.java
@@ -1,31 +1,46 @@
 package io.github.lsj8367.demo.example.web;
 
-import static org.junit.jupiter.api.Assertions.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import io.github.lsj8367.demo.example.configuration.LoggingAspect;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.context.annotation.Import;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
-import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
-import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
 
 @Import(LoggingAspect.class)
 @WebMvcTest(SampleController.class)
 class SampleControllerTest {
 
-    @Autowired
     private MockMvc mockMvc;
 
-    @Test
-    void helloTest() throws Exception {
-        mockMvc.perform(get("/api/v1/hello"))
-            .andExpect(status().isOk())
-            .andDo(print());
+    @Autowired
+    WebApplicationContext ctx;
+
+    @BeforeEach
+    void setUp() {
+        mockMvc = MockMvcBuilders.webAppContextSetup(ctx)
+            .alwaysDo(print())
+            .build();
     }
 
+    @Test
+    @DisplayName("hello Controller 테스트")
+    void helloTest() throws Exception {
+        mockMvc.perform(get("/api/v1/hello"))
+            .andExpect(status().isOk());
+    }
+
+    @Test
+    @DisplayName("around aop 테스트")
+    void aroundTest() throws Exception {
+        mockMvc.perform(get("/api/v1/around"))
+            .andExpect(status().isOk());
+    }
 }
